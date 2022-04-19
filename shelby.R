d8dcad534c0cc19cdf58466ee0b3198307015f4f
Shelby Pankratz <pa4039pb@mnstate.edu>
2022-04-19 13:07:45 -0500

    end of 4/19 lab

--- shelby.R
+++ shelby.R
@@ -6,20 +6,23 @@ citation("tidyverse")
 
 if (!dir.exists("figs")) dir.create("figs")
 
+age_group_levels <- c("<5", "5-11", "<12", "16-17", "18-24", 
+                      "25-39", "40-49", "50-64", "65-74", "75+")
+
 ## vaccination data ----
 # raw data ----
 vax_data <- 
-  read_csv("data/COVID-19_Vaccination_Demographics_in_the_United_States_National (1).csv") %>% 
-  filter(
-    str_starts(Demographic_category, "Ages_")
-  ) %>% 
+  read_csv("data/COVID-19_Vaccination_Demographics_in_the_United_States_National.csv") %>% 
+  filter(str_starts(Demographic_category, "Ages_")) %>%
+rename(age_group = Demographic_Category) %>% 
   mutate(
     Date = as.Date(Date, format = "%m/%d/%Y"),
-    age_group=str_remove(Demographic_category, "Ages_"),
+    age_group=str_remove(age_group, "Ages_"),
     age_group=str_remove(age_group, "_yrs"),
+    age_group=str_remove(age_group, "yrs")
   ) %>% 
   filter(!age_group %in% c("<12yrs", "<5yrs", "12-15", "16-17")) %>% 
-  mutate(age_group = factor(age_group, levels = order)) %>%
+  mutate(age_group = factor(age_group, levels = age_group_levels)) %>%
   rename(
     per_vax = Series_Complete_Pop_pct_agegroup, 
     date = Date
